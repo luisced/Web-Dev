495532116fdb8b1b14a59cad5d9fd63f412fdbb1
luisced <luis.cedillo.maldonado@gmail.com>
2024-09-13 19:36:19 -0600

    fix for click event

--- Cryptic/js/script.js
+++ Cryptic/js/script.js
@@ -14,15 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
     addCircleClickEvent('circle3');
     addCircleClickEvent('circle4');
     addCircleClickEvent('circle5');
-
-    // Add click event listener to the document to deselect circle when clicking elsewhere
-    document.addEventListener('click', (event) => {
-        // If a circle is selected and the click is outside a circle, deselect the circle
-        if (selectedCircle && !event.target.closest('.circle')) {
-            selectedCircle.style.borderColor = '';  // Reset the border color
-            selectedCircle = null;  // Deselect the circle
-        }
-    });
 });
 
 // Distribute letters evenly around the circle
@@ -53,14 +44,17 @@ function distributeLetters(circleId, letterCount) {
 // Add click event listener to each circle to make it selectable
 function addCircleClickEvent(circleId) {
     const circle = document.getElementById(circleId);
-    circle.addEventListener('click', (event) => {
-        event.stopPropagation(); // Prevent triggering the document click listener
-        // Deselect any previously selected circle
-        if (selectedCircle) {
-            selectedCircle.style.borderColor = ''; // Reset the border color of previously selected circle
-        }
-        selectedCircle = circle;  // Set the newly selected circle
+    circle.addEventListener('click', () => {
+        selectedCircle = circle;  // Set the selected circle
         circle.style.borderColor = 'red';  // Change border color to indicate selection
+        // when other circle is selected, remove the border color
+        const allCircles = document.querySelectorAll('.circle');
+        allCircles.forEach((c) => {
+            if (c !== circle) {
+                c.style.borderColor = 'black';
+            }
+        });
+
     });
 }
 
@@ -88,6 +82,8 @@ function normalizeAngle(angle) {
 
 // Check if all circles are correctly aligned based on valid angles and redirect to success
 function checkAlignment() {
+    // pswd: ACGHF
+
     let allCorrect = true;
 
     // Valid angles for circle1
